82bfd9cda2d243e793724ff468460ae29270b7ff
Tushar Pathade <pathadetc19.comp@coep.ac.in>
2021-06-14 02:48:33 +0530

    Added snaps

--- Whack-a-Mole/app.js
+++ Whack-a-Mole/app.js
@@ -1,57 +1,56 @@
-const square = document.querySelectorAll('.square')
-const mole = document.querySelectorAll('.mole')
-const timeLeft = document.querySelector('#time-left')
-const btn = document.querySelector('.btn')
-let score = document.querySelector('#score')
+const square = document.querySelectorAll(".square");
+const mole = document.querySelectorAll(".mole");
+const timeLeft = document.querySelector("#time-left");
+const btn = document.querySelector(".btn");
+let score = document.querySelector("#score");
 
- btn.addEventListener("click", () => {
-   location.reload();
- });
+btn.addEventListener("click", () => {
+  location.reload();
+});
 
-let result = 0
-let currentTime = timeLeft.textContent
+let result = 0;
+let currentTime = timeLeft.textContent;
 
 function randomSquare() {
-    // removing img from each box
-    square.forEach(className => {
-        className.classList.remove('mole')
-    })
+  // removing img from each box
+  square.forEach((className) => {
+    className.classList.remove("mole");
+  });
 
-    // taking random position of box and adding img to it
-    let randomPosition = square[Math.floor(Math.random() * 9)]
-    randomPosition.classList.add('mole')
+  // taking random position of box and adding img to it
+  let randomPosition = square[Math.floor(Math.random() * 9)];
+  randomPosition.classList.add("mole");
 
-    // assign the id of the randomPosition to hitPosition for us to use later
-    hitPosition = randomPosition.id
+  // assign the id of the randomPosition to hitPosition for us to use later
+  hitPosition = randomPosition.id;
 }
 
-square.forEach(id => {
-    id.addEventListener('mouseup', () => {
-        if (id.id == hitPosition) {
-            result += 1
-            score.textContent = result
-        }
-    })
-})
+square.forEach((id) => {
+  id.addEventListener("mouseup", () => {
+    if (id.id == hitPosition && currentTime != 0) {
+      result += 1;
+      score.textContent = result;
+    }
+  });
+});
 
 let timerIdRSquare = null;
 function moveMole() {
-    timerIdRSquare = setInterval(randomSquare, 1000);
+  timerIdRSquare = setInterval(randomSquare, 1000);
 }
-
 
 function countDown() {
-    currentTime--
-    timeLeft.textContent = currentTime
+  currentTime--;
+  timeLeft.textContent = currentTime;
 
-    if (currentTime == 0) {
-        clearInterval(timerIdRSquare)
-        clearInterval(timerId)
-        clearInterval()
-        alert('Game Over! Your final score is: '+ result)
-    }
+  if (currentTime == 0) {
+    clearInterval(timerIdRSquare);
+    clearInterval(timerId);
+    clearInterval();
+    alert("Game Over! Your final score is: " + result);
+  }
 }
 
-let timerId = setInterval(countDown, 1000)
+let timerId = setInterval(countDown, 1000);
 
-moveMole()
+moveMole();
